7fcc53b64ca1a0ca7d1e1c11bf392303b7211fa9
motlalepulag <motlalepulag.phiri@gmail.com>
2023-02-24 06:31:08 -0800

    updated

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -7,8 +7,9 @@
  */
 int main(void)
 {
-    print_sqaure(2);
-    print_sqaure(10);
-    print_sqaure(0);
-    return (0);
+print_triangle(2);
+print_triangle(10);
+print_triangle(1);
+print_triangle(0);
+return (0);
 }
