eac1a098d5e1adc2d0a67681e09219e7043b309e
motlalepulag <motlalepulag.phiri@gmail.com>
2023-02-27 18:52:14 -0800

    updated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,12 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int n = 0;
-	int signo = 1;
+	int signo = -1;
 
 	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
 		if (s[i] == '_')
-			signo *= 1;
+			signo *= -1;
 		i++;
 	}
 	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
