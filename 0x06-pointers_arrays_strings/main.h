5edda966a329e32df0acb237185e8c6062b8d810
motlalepulag <motlalepulag.phiri@gmail.com>
2023-03-03 00:18:38 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
-int main(void);
+int *_main(void);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 
