e0b56f5a360e077e06b705224c3f34692a2c6dee
motlalepulag <motlalepulag.phiri@gmail.com>
2023-03-03 05:35:10 -0800

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,21 +3,22 @@
  * *_strcmp - compare two strings
  * @s1: string 1
  * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
+ *
+ * Return: s1[i] - s2[i]
  */
 int *_strcmp(char *s1, char *s2)
 {
 	int i;
 
-	/**
-	 * we have reached the end of S1 or we have reached the end of S2
-	 */
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+
+	while (s1[i] != '\0' &&  s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
 	return (0);
 }
