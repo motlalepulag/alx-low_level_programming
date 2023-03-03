dbc501b01f578335c93f47373e22ea59ecbe0922
motlalepulag <motlalepulag.phiri@gmail.com>
2023-03-03 07:34:42 -0800

    102-magic.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,14 @@
 
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < (n - 1); i++)
+	int i = 0;
+	
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
-	{
-		if (i == (n - 1))
-		{
-			printf("%d", a[n - 1]);
-		}
+		printf("i%d", a[i]);
+		if (i < n - 1)
+			printf(",");
 	}
 	printf("\n");
 }
-}
+
