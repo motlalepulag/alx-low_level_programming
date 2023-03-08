fac68d7e12bc5780734f9eac64b69e917d19671d
motlalepulag <motlalepulag.phiri@gmail.com>
2023-03-08 04:22:21 -0800

    updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,15 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: The string to be printed
+ *
+ * Return: void
  */
+
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
-		_putchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else
-		_putchar('\n');
 }
