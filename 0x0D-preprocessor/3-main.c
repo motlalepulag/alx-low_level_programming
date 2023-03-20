aa47f0b7c10c23c2a507521568699b44609e15be
motlalepulag <motlalepulag.phiri@gmail.com>
2023-03-20 04:04:54 -0700

    updated

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -5,16 +5,15 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Reurn: Always 0
  */
-
 int main(void)
 {
 	int i;
-	 int j;
+	int j;
 
-	 i = ABS(-98) * 10;
-	 j = ABS(98) * 10;
-	 printf("%d, %d\n", i, j);
-	 return (0);
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
+	return (0);
 }
